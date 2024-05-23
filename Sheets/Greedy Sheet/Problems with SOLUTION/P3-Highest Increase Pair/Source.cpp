a2c3fee3f88b2c2c31c954d6da032cd9cc315ff6
Bishoy Sedra <bishosedra0@gmail.com>
2024-05-23 14:30:37 +0300

    Made Some Changes!

--- Sheets/Greedy Sheet/Problems with SOLUTION/P3-Highest Increase Pair/Source.cpp
+++ Sheets/Greedy Sheet/Problems with SOLUTION/P3-Highest Increase Pair/Source.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <math.h>
-#include<queue>
-#include <algorithm> 
+#include <queue>
+#include <algorithm>
 #include <map>
 #include <math.h>
 #include <stack>
@@ -9,19 +9,22 @@
 using namespace std;
 #define ll long long
 
-int main() {
-	int arr[100] = { 31,-41,59,26,-53,58,97,-93,-23,84 };
+int main()
+{
+	int arr[100] = {31, -41, 59, 26, -53, 58, 97, -93, -23, 84};
 	int index = 0;
 	int s = 0, e = 0;
 	int maxx = -1000;
 	for (int j = 1; j < 10; j++)
 	{
-		if (arr[j] < arr[index]) {
+		if (arr[j] < arr[index])
+		{
 			index = j;
 			continue;
 		}
 		int dif = arr[j] - arr[index];
-		if (maxx < dif) {
+		if (maxx < dif)
+		{
 			maxx = dif;
 			s = index;
 			e = j;
